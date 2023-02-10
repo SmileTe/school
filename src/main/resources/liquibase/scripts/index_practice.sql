8efe82944e21a77a044ae246df2d6adc06b30b22
grand <ferthlin@ya.ru>
2023-02-10 10:28:29 +0300

    дз 4_3 вар.2

--- src/main/resources/liquibase/scripts/index_practice.sql
+++ src/main/resources/liquibase/scripts/index_practice.sql
@@ -1,6 +1,8 @@
- --liquibase formatted sql
- --changeset msalimgareeva:1
- CREATE INDEX student_name_index ON student(name);
+ -- liquibase formatted sql
+ -- changeset msalimgareeva:1
+ CREATE INDEX student_name_index ON student (name);
+
+ CREATE INDEX faculty_name_color_index ON faculty (name,color);
 
 
 
